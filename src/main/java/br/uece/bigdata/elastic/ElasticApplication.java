a942e4014e02ef867538a617c05865c58a1a6acb
DUDU-E-NATTY\eduma <edumaia92@gmail.com>
2025-02-23 22:56:06 -0300

    First commit

---
+++ src/main/java/br/uece/bigdata/elastic/ElasticApplication.java
@@ -0,0 +1,13 @@
+package br.uece.bigdata.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uece/bigdata/elastic/ElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uece.bigdata.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
